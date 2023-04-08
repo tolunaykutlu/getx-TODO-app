9048315faa5b3b9b6e4e765271c270bff41fb8a9
tolunaykutlu <tolunaykt28@gmail.com>
2023-04-08 23:29:08 +0300

    bir kaç değişiklik sonrası

--- lib/util/counter.dart
+++ lib/util/counter.dart
@@ -2,15 +2,34 @@ import 'package:get/get.dart';
 
 class Controller extends GetxController {
   var count = 0.obs;
-  List<String> todo = ["yemek ", "içmek", "uyumak"].obs;
+  List todo = ["yemek", "içmek", "uyumak"].obs;
+  // with 2 lists we can achieve the goal :D
+  List isDone = [false, false, false].obs;
 
   increment() {
     count++; //count.value for if else statements
     update();
   }
 
-  addListItem(String value) {
-    todo.add(value);
+  checkBoxChanged(int index) {
+    isDone[index] = !isDone[index];
+
+    update();
+  }
+
+//add Task
+  addTask(String task, bool value) {
+    todo.add(task);
+    isDone.add(value);
+    update();
+    Get.snackbar("Görev Başarıyla Eklendi", task,
+        duration: const Duration(seconds: 1));
+  }
+
+//delete task
+  deleteTask(int index) {
+    todo.removeAt(index);
+    isDone.removeAt(index);
     update();
   }
 }
